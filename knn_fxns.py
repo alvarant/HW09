0bfe887da32040257a020a640a5e9a0782de5282
Anton Alvarez <alvarant@oregonstate.edu>
2021-06-09 05:42:00 +0000

    made functions

--- knn_fxns.py
+++ knn_fxns.py
@@ -8,7 +8,10 @@ def row_distance(row1, row2):
     INPUT: two rows which should be Pandas series or array-type, not data frame 
     OUTPUT: Euclidean disstance
     '''
-    
+    pt1=np.array(row1)
+    pt2=np.array(row2)
+    dist=np.sqrt(sum((pt1-pt2)**2))
+    return dist
 
 def calc_distance_to_all_rows(df,example_row ):
     '''
@@ -17,8 +20,16 @@ def calc_distance_to_all_rows(df,example_row ):
     INPUT: df, Pandas dataframe; example_row
     OUTPUT:Pandas dataframe with additional column 'distance_to_ex' added to input dataframe df
     '''
-    
-    
+    distances=[]
+    attribute_df=df.drop(['class'],axis=1)
+    num_rows=df.shape[0]
+    for row_num in np.arange(num_rows):
+        current_row=attribute_df.iloc[row_num,:]
+        current_distance=row_distance(current_row,example_row)
+        distances.append(current_distance)
+    return_df=df.assign(distance_to_ex=distances)
+    return return_df
+        
 
 def find_k_closest(df, example_row, k):
     """
@@ -28,8 +39,18 @@ def find_k_closest(df, example_row, k):
     INPUT: df, Pandas dataframe; example_row, Pandas series or array type; k, integer number of nearest neighbors.
     OUTPUT: dataframe in same format as input df but with k rows and sorted by 'distance_to_ex.'
     """
-    
-    
+    ret_df=calc_distance_to_all_rows(df,example_row)
+    sort_df=ret_df.sort_values(by=['distance_to_ex'])
+    return sort_df[0:k]
+
+def get_sorted_k_counts(ksorted_df):
+    grouped_df=ksorted_df.groupby(['class']).size().reset_index(name='counts')
+    sorted_df=grouped_df.sort_values(by=['counts'],ascending=False)
+    return sorted_df
+
+def majority_class(sorted_df):
+    return sorted_df['class'].iloc[0]
+
 def classify(df, example_row, k):
     """
     Return the majority class from the k nearest neighbors of example
@@ -37,4 +58,19 @@ def classify(df, example_row, k):
     INPUT: df, Pandas dataframe; example_row, Pandas series or array type; k, integer number of nearest neighbors
     OUTPUT: string referring to closest class.
     """
+    k_df= find_k_closest(df,example_row,k)
+    sorted_df=get_sorted_k_counts(k_df)
+    return majority_class(sorted_df)
+
+def evaluate_accuracy(training_df, test_df,k):
+    '''Returns the proportion of the test_df that was correctly classified'''
+    test_attribute_df=test_df.drop(['class'],axis=1)
+    num_correct=0
+    num_rows=test_df.shape[0]
     
+    for row_num in np.arange(num_rows):
+        example_row=test_attribute_df.iloc[row_num,:]
+        c=classify(training_df,example_row,k)
+        if c==test_df['class'].iloc[row_num]:
+            num_correct=num_correct+1
+    return num_correct/num_rows
